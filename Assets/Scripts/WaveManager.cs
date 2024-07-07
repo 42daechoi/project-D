7fcf8916c919bc3a1d9361290f36c85ec7fa84e5
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-07-07 18:49:46 +0900

    PD-5 <웨이브매니저 및 시간콘솔 출력>

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class WaveManager : MonoBehaviour
 {
     public GameObject[] monsterPrefabs;  // 스폰할 몬스터 프리팹 배열
     public Transform spawnPoint;  // 스폰 포인트
     public Transform[] waypoints;  // 웨이포인트 배열
     public float spawnInterval = 0.5f;  // 스폰 간격
+    public float timeLimit = 30f;
+    private float currentTime;
+    private int lastDisplayedTime;
 
     private int monsterIndex = 0;
     private int spawnCount = 0;
@@ -16,9 +19,32 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentTime = timeLimit;
+        lastDisplayedTime = Mathf.CeilToInt(currentTime); // 초기화 시 현재 시간을 정수로 저장
+        StartCoroutine(StartTimer());
         StartCoroutine(SpawnMonsters());
     }
 
+    IEnumerator StartTimer()
+    {
+        while (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+
+            int ceilCurrentTime = Mathf.CeilToInt(currentTime);
+            // 초 단위로 시간이 변할 때마다 콘솔에 출력
+            if (ceilCurrentTime != lastDisplayedTime)
+            {
+                Debug.Log($"Time: {ceilCurrentTime}");
+                lastDisplayedTime = ceilCurrentTime; // 마지막으로 출력한 시간을 업데이트
+            }
+
+            yield return null;
+        }
+
+        Timeout();
+    }
+    
     IEnumerator SpawnMonsters()
     {
         while (true)
@@ -49,5 +75,10 @@ public class SpawnManager : MonoBehaviour
             // 스폰 간격 관련 코루틴
             yield return new WaitForSeconds(spawnInterval);
         }
+    }
+
+    void Timeout()
+    {
+        Debug.Log("타임아웃");
     }
 }
