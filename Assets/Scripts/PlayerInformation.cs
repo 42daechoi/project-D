bd342cf2aea3e030f42494216b38ba2665cb555b
42 <daechoi@student.42seoul.kr>
2024-07-07 17:37:43 +0900

    PD-10 <Player Level UI 재구성 및 Level Up 구현>

--- Assets/Scripts/PlayerInformation.cs
+++ Assets/Scripts/PlayerInformation.cs
@@ -7,28 +7,58 @@ public class PlayerInformation : MonoBehaviour
 {
     public int gold;
     public int level;
+    public int goldForLevelUp;
 
     public TextMeshProUGUI goldText;
     public TextMeshProUGUI levelText;
+    public TextMeshProUGUI goldForLevelUpText;
 
     // Start is called before the first frame update
     void Start()
     {
-        gold = 0;
+        gold = 50;
         level = 1;
-        UpdateGold();
-        UpdateLevel();
+        goldForLevelUp = 20;
     }
 
-    public void UpdateGold()
+    void Update()
     {
-        gold++;
+        UpdateGoldText();
+        UpdateLevelText();
+        UpdateGoldForLevelUpText();
+    }
+
+    public void TryLevelUp()
+    {
+        if (gold >= goldForLevelUp)
+        {
+            gold -= goldForLevelUp;
+            UpdateGoldForLevelUp();
+            level++;
+        }
+        else
+        {
+            Debug.Log("골드가 부족합니다.");
+        }
+    }
+
+    void UpdateGoldForLevelUp()
+    {
+        goldForLevelUp += (level * goldForLevelUp);
+    }
+
+    public void UpdateGoldText()
+    {
         goldText.text = "Gold : " + gold.ToString();
     }
 
-    public void UpdateLevel()
+    public void UpdateLevelText()
     {
-        level++;
         levelText.text = "Level : " + level.ToString();
+    }
+
+    public void UpdateGoldForLevelUpText()
+    {
+        goldForLevelUpText.text = "GoldForLevelUp : " + goldForLevelUp.ToString();
     }
 }
