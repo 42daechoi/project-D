9c133c218febaf74f8bd66f4e7fbc26eedfd7308
42daechoi <93859760+42daechoi@users.noreply.github.com>
2024-07-08 20:50:58 +0800

    Revert "PD-5 <Monster wave 재정의 및 구현>"

--- Assets/Scripts/Wave.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class Wave
-{
-    public GameObject[] enemies;  // 웨이브에서 스폰할 적 프리팹 배열
-    public int enemyCount = 10;        // 웨이브당 적의 수
-    public float spawnInterval = 0.5f; // 적 스폰 간격
-}
