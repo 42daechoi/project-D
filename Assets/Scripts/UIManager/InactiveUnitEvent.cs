f2179148844d2349a1bad74c4c1473b2b26490de
42 <daechoi@student.42seoul.kr>
2024-07-16 16:23:30 +0900

    PD-22 <활성화된 유닛 비활성화 후 드래그앤드롭 실패 시 다른 위치 배치>

--- Assets/Scripts/UIManager/InactiveUnitEvent.cs
+++ Assets/Scripts/UIManager/InactiveUnitEvent.cs
@@ -20,23 +20,21 @@ public class InactiveUnitEvent : MonoBehaviour
 	{
 		GameObject[] selectedUnits = GameManager.Instance.GetSelectedUnits();
 		GameObject[] inactiveUnitGrounds = GameObject.FindGameObjectsWithTag("InactiveUnitGround");
-		Debug.Log(inactiveUnitGrounds.Length);
-
 
 		if (selectedUnits == null) return ;
 
 		foreach (GameObject unit in selectedUnits)
 		{
             UnitAbilites ua = unit.GetComponent<UnitAbilites>();
-            foreach (GameObject iug in inactiveUnitGrounds)
+            for(int i = inactiveUnitGrounds.Length - 1; i >= 0; i--)
 			{
-				VerifyActivation verifyActivation = iug.GetComponent<VerifyActivation>();
+				VerifyActivation verifyActivation = inactiveUnitGrounds[i].GetComponent<VerifyActivation>();
 				if (!verifyActivation.GetActivation())
 				{
-                    Transform iugTransform = iug.transform;
+                    Transform iugTransform = inactiveUnitGrounds[i].transform;
 					Vector3 placePosition = iugTransform.position + new Vector3(0, 1, 0);
 
-                    ua.SetActivation(iug);
+					ua.SetActivation(inactiveUnitGrounds[i]);
                     unit.transform.position = placePosition;
 					break ;
 				}
