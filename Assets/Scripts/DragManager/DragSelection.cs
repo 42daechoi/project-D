eb80586e7fc79c7d448cf39f686832c462723397
42 <daechoi@student.42seoul.kr>
2024-09-02 03:14:38 +0900

    PD-37 <A 클릭 후 마우스 좌클릭 시 DragSelection이 실행되는 현상>

--- Assets/Scripts/DragManager/DragSelection.cs
+++ Assets/Scripts/DragManager/DragSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,57 +6,57 @@ using UnityEngine.UI;
 
 public class DragSelection : MonoBehaviour
 {
-    public Image selectionBoxImage;
-    private UnitSelector unitSelector;
-    private RectTransform selectionBoxRect;
+	public Image selectionBoxImage;
+	private UnitSelector unitSelector;
+	private RectTransform selectionBoxRect;
 
-    private Vector2 startPosition;
-    private Vector2 endPosition;
-    private bool isInAllowArea;
+	private Vector2 startPosition;
+	private Vector2 endPosition;
+	private bool isInAllowArea;
 
-    void Start()
-    {
-        selectionBoxRect = selectionBoxImage.GetComponent<RectTransform>();
-        unitSelector = GetComponent<UnitSelector>();
-        selectionBoxImage.gameObject.SetActive(false);
-    }
+	void Start()
+	{
+		selectionBoxRect = selectionBoxImage.GetComponent<RectTransform>();
+		unitSelector = GetComponent<UnitSelector>();
+		selectionBoxImage.gameObject.SetActive(false);
+	}
 
-    public void UpdateDragSelection()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isInAllowArea = Input.mousePosition.x > 400 && Input.mousePosition.x < 1500 && Input.mousePosition.y > 160;
-            if (isInAllowArea)
-            {
-                startPosition = Input.mousePosition;
-                selectionBoxImage.gameObject.SetActive(true);
-            }
-        }
-        if (Input.GetMouseButton(0) && isInAllowArea)
-        {
-            endPosition = Input.mousePosition;
-            UpdateSelectionBox();
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (isInAllowArea) 
-            {
-                unitSelector.SelectUnits(selectionBoxRect);
-                selectionBoxImage.gameObject.SetActive(false);
-            }
-            isInAllowArea = false;
-        }
-    }
+	public void UpdateDragSelection()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			isInAllowArea = Input.mousePosition.x > 400 && Input.mousePosition.x < 1500 && Input.mousePosition.y > 160;
+			if (isInAllowArea)
+			{
+				startPosition = Input.mousePosition;
+				selectionBoxImage.gameObject.SetActive(true);
+			}
+		}
+		if (Input.GetMouseButton(0) && isInAllowArea)
+		{
+			endPosition = Input.mousePosition;
+			UpdateSelectionBox();
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (isInAllowArea)
+			{
+				unitSelector.SelectUnits(selectionBoxRect);
+				selectionBoxImage.gameObject.SetActive(false);
+			}
+			isInAllowArea = false;
+		}
+	}
 
-    void UpdateSelectionBox()
-    {
-        Vector2 boxStart = startPosition;
-        Vector2 boxEnd = endPosition;
+	void UpdateSelectionBox()
+	{
+		Vector2 boxStart = startPosition;
+		Vector2 boxEnd = endPosition;
 
-        float width = boxEnd.x - boxStart.x;
-        float height = boxEnd.y - boxStart.y;
+		float width = boxEnd.x - boxStart.x;
+		float height = boxEnd.y - boxStart.y;
 
-        selectionBoxRect.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
-        selectionBoxRect.anchoredPosition = boxStart + new Vector2(width / 2, height / 2);
-    }
+		selectionBoxRect.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+		selectionBoxRect.anchoredPosition = boxStart + new Vector2(width / 2, height / 2);
+	}
 }
